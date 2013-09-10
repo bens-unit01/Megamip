cbbd0497d81f4fc175ec82a35038f98d056f152c
bens-unit01 <bensalem.messaoud@gmail.com>
2013-09-10 16:49:40 -0400

    added motors control to com_moteur_1.ino

--- Arduino/com_moteur_1/com_moteur_1.ino
+++ Arduino/com_moteur_1/com_moteur_1.ino
@@ -12,9 +12,15 @@
 #define BPWM 6   
 #define stby 13
 
-SoftwareSerial mySerial(10, 11); // RX, TX
-  int GestureData[6];
+#define DEBUG 12
+
+// SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(10, DEBUG); // RX, TX
+int GestureData[6];
 int turnData = 0;
+int deb01 =0, deb02 = 0;
+boolean newInput = false;   
+byte buffer[200];           // buffer for the serial input 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////SETUP///////////////////////////////////////////////////
@@ -28,7 +34,7 @@ void setup()
 
 
  // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+  mySerial.begin(115200);
   
   //Debug pin for timing analysis
   pinMode(A0,OUTPUT); 
@@ -55,6 +61,10 @@ void setup()
  // Turn motor drivers off from standby
  digitalWrite(stby,LOW); 
  
+ 
+ //////////////////////////////
+
+ 
 }
 
 
@@ -62,44 +72,58 @@ void setup()
 //////////////////////////////////////////////Main Loop///////////////////////////////////////////////////////////  
 void loop()
 {
-  //Serial.print("hi");
+  // Serial.print("hi");
   int i=0;
-
- 
-while (mySerial.available())
-{
- // Serial.write(mySerial.read());
   
-      if (mySerial.read() == 0xFC)
-      {
-        while (mySerial.available())
-        {
-          GestureData[i] = float(mySerial.read());
-          i++;
-  
-          if (i>1)
+// gesture reading  
+    while (mySerial.available())
+    {
+     // Serial.write(mySerial.read());
+      
+          if (mySerial.read() == 0xFC)
           {
-            ProcessSwipe();
-
-            i=0;
-            return;
+            while (mySerial.available())
+            {
+              GestureData[i] = float(mySerial.read());
+              i++;
+      
+              if (i>1)
+              {
+               // ProcessSwipe();                               // commented to use the soft serial for debugging -------------------------------------------
+    
+                i=0;
+                return;
+              }
+            }   
           }
-        }   
-      }
-       
-      
      
-      
 }
-  
-    RC_Drive(0.0, 0.5, 100);
-    
-    
-    
-    // param 1 : vitesse -1 to 1
-    // param 2 : vitesse differentiel  param1 - param2 = mot guach, param1 + param2 = mot droite  
-    // temps : miliseconde 
 
+
+// input handling ---------------------------------------
+     
+    if(newInput){
+          mipHandler();
+          newInput = false;
+         
+    }    
+     
+   
+   
+//---------------DEBUG--------------------------------
+#ifdef DEBUG
+      deb01++;
+      if((deb01 % 30000) == 0) {
+        deb01 = 0; deb02++;
+        
+         mySerial.print(deb02,DEC);
+         mySerial.println(" : debug  - loop");
+         
+      }
+      
+#endif
+//---------------------------------------------------
+ 
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -190,6 +214,12 @@ void DebugOutput()
 }
 
 
+
+
+
+// param 1 u    : vitesse -1 to 1
+// param 2 turn : vitesse differentiel  param1 - param2 = mot guach, param1 + param2 = mot droite  
+// param 3 time : milliseconde 
 void RC_Drive(float u, float turn, float time)
   {
     
@@ -225,3 +255,131 @@ void RC_Drive(float u, float turn, float time)
   }
   
 
+void serialEvent() {
+  
+  
+  
+//---------------DEBUG-------------------
+#ifdef DEBUG
+   mySerial.println("debug  - serialEvent 0");
+#endif
+//-----------------------------------------  
+   
+      
+
+          int i= 0;
+          // reading  from the serial port buffer
+          while (Serial.available() && i < sizeof(buffer)) {
+            // get the new byte:
+            buffer[i] = Serial.read();
+            i++;
+            
+          }
+          newInput = true;
+       
+  
+}
+
+void mipHandler(){
+
+  
+ 
+//---------------DEBUG-------------------
+#ifdef DEBUG
+   mySerial.println("debug  - mipHandler 0");
+#endif
+//-----------------------------------------  
+     
+   // decoding the data 
+          
+          // we need the start byte (0xFE ) index,  
+          int i = 0;
+          while(buffer[i] != 0xFE)  i++; 
+          byte cmd = buffer[i+1];
+          byte arg1 = 0;
+          byte arg2 = 0;
+          byte arg3 = 0;
+          byte arg4 = 0;
+
+     switch(cmd){
+      
+      case 0x10 : // drive
+                    
+                    arg1 = buffer [i+2];
+                    arg2 = buffer [i+3];
+                    arg3 = buffer [i+4];
+                    arg4 = buffer [i+5];
+                    drive(arg1, arg2, arg3, arg4);
+                    delay(500);
+//---------------DEBUG-----------------------
+#ifdef DEBUG
+                  
+                    mySerial.println("debug - serialEvent  1");
+                    mySerial.print(" cmd: ");
+                    mySerial.print(cmd, DEC);
+                    mySerial.print(" args: ");
+                    mySerial.print(arg1, DEC);
+                    mySerial.print("\t");
+                    mySerial.print(arg2, DEC);
+                    mySerial.print("\t");
+                    mySerial.print(arg3, DEC);
+                    mySerial.print("\t");
+                    mySerial.println(arg4, DEC);
+                
+#endif
+//-----------------------------------------------
+                    break;
+      case 0x11 : // driveQueue 
+                    arg1 = buffer [i+2];
+                    arg2 = buffer [i+3];
+                    arg3 = buffer [i+4];
+                    arg4 = buffer [i+5];
+                    // call to driveQueue(arg1, arg2, arg3, arg4)
+                   break;
+        
+      case 0x12 : // upritht  
+                    // call to upright()
+                   break;
+      case 0x13 : // upritht  
+                    // call to fallover()
+                   break;
+      case 0x14 : // upritht  
+                    // call to stop()
+                   break;
+      case 0x15 : // upritht  
+                    // call to reportEncoders()
+                   break;
+     default :  break;
+      
+      
+      
+      
+      
+      }
+
+
+}
+
+void drive(int speedL, int speedR, int distanceL, int distanceR){
+  
+  
+  
+   float u = (float)speedL / 100;
+   float turn = 0.5; 
+   float time = distanceL * 100;
+   
+   
+//---------------DEBUG-----------------------
+#ifdef DEBUG
+                    mySerial.println("debug - drive");
+                    mySerial.print(" u: ");
+                    mySerial.print(u, 6);
+                    mySerial.print(" turn: ");
+                    mySerial.print(turn, 6);
+                    mySerial.print("\t");
+                    mySerial.print(" time:");
+                    mySerial.println(time, 6);                
+#endif
+//-----------------------------------------------
+    RC_Drive(u, turn, time);
+}
